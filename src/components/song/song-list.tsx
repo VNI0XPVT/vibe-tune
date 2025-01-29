6d295e127703a5ebb5d16a4021c71177b2a0371f
devxprite <devxprite@gmail.com>
2025-01-29 12:10:57 +0530

    Update

--- src/components/song/song-list.tsx
+++ src/components/song/song-list.tsx
@@ -2,6 +2,7 @@ import { Heart, ListPlusIcon, MinusCircleIcon, PlayCircle, PlusCircle } from 'lu
 import { cn, formatDuration } from '../../lib/utils';
 import songs from '../../data/songs-data';
 import { useMusicPlayerContext } from '../../context/audio-provider';
+import { Audio, Bars } from 'react-loader-spinner';
 
 type Props = {
     number?: number;
@@ -48,14 +49,11 @@ const SongList = ({ song, number, showAlbum = false }: Props) => {
 
     const Duration = () => <p className="text-xs md:text-sm pr-1">{formatDuration(song.duration)}</p>;
 
-    const SongWithoutAlbum = () => (
+    const SongWithoutAlbum = (
         <>
-            <p className="opacity-80 w-4 text-center">
-                {number && <span className="group-hover:hidden">{number}</span>}
-                <span className="hidden group-hover:inline">
-                    <PlayCircle className="size-5 md:size-5" />
-                </span>
-            </p>
+            <div className="w-4 text-center flex items-center justify-center text-sm">
+                {isCurrentSong ? <Bars color="white" height={16} width={16} /> : <span>{number}</span>}
+            </div>
 
             <p className="flex-1 max-md:text-sm font-semibold text-foreground/90">{song.name}</p>
             <Artist />
@@ -64,9 +62,17 @@ const SongList = ({ song, number, showAlbum = false }: Props) => {
         </>
     );
 
-    const SongWithAlbum = () => (
+    const SongWithAlbum = (
         <>
-            <img src={song.image} className="border rounded-md size-11 md:size-11 " />
+            <div className="size-11 md:size-11 relative border rounded-md overflow-hidden">
+                <img src={song.image} className=" rounded-md size-full" />
+
+                {isCurrentSong && (
+                    <div className="absolute inset-0 flex items-center justify-center bg-black/60">
+                        <Bars color="white" height={20} width={20} />
+                    </div>
+                )}
+            </div>
 
             <div className="flex-1 flex flex-col justify-around">
                 <h4 className="font-semibold text-foreground/90 line-clamp-1 text-ellipsis">{song.name}</h4>
@@ -81,16 +87,14 @@ const SongList = ({ song, number, showAlbum = false }: Props) => {
 
     return (
         <div
-            // role="button"
             className={cn(
                 'flex justify-between gap-2 p-2 md:gap-4 group cursor-pointer items-center',
                 'rounded-md text-muted-foreground hover:bg-muted hover:shadow-md transition-all duration-300',
                 showAlbum ? 'p-1.5' : ''
-                // 'bg-red-950'
             )}
             onClick={() => playSong(song)}
         >
-            {showAlbum ? <SongWithAlbum /> : <SongWithoutAlbum />}
+            {showAlbum ? SongWithAlbum : SongWithoutAlbum}
         </div>
     );
 };
